9e98d6646a5fe11d1a109605738af382fda0d07f
Steve Berdy <86739818+steveberdy@users.noreply.github.com>
2025-02-04 22:41:46 -0500

    UI work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,63 @@
 import "./App.css";
+import {
+  ThemeProvider, AnchorNav, CTABanner, Button, IDE, Hero
+} from "@primer/react-brand";
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <ThemeProvider
+    colorMode="auto"
+    style={{ backgroundColor: 'var(--brand-color-canvas-default)' }}
+  >
+    <AnchorNav enableDefaultBgColor>
+      <AnchorNav.Link href="about">About</AnchorNav.Link>
+      <AnchorNav.Link href="products">Products</AnchorNav.Link>
+      <AnchorNav.Link href="labs">Labs</AnchorNav.Link>
+      <AnchorNav.Link href="blog">Blog</AnchorNav.Link>
+      <AnchorNav.Link href="contact">Contact</AnchorNav.Link>
+      <AnchorNav.Action href="register">Sign up</AnchorNav.Action>
+    </AnchorNav>
+    <CTABanner
+      backgroundImageSrc={{
+        narrow: "images/hero.png",
+        regular: "images/hero.png",
+        wide: "images/hero.png"
+      }}
+      align="center"
+    >
+      <CTABanner.Heading className="">
+        From “we use AI” to ROI
+      </CTABanner.Heading>
+      <CTABanner.Description variant="default">
+        <b>
+          Deliver lasting value to your customers.<br />When AI becomes ubiquitous, it will
+          be the value you deliver with it that matters most.
+        </b>
+      </CTABanner.Description>
+      <CTABanner.ButtonGroup>
+        <Button>I'm interested</Button>
+        <Button>I'm not interested</Button>
+      </CTABanner.ButtonGroup>
+    </CTABanner>
+    <Hero />
+    <CTABanner>
+      <CTABanner.Heading size="3">
+        Hype is great.<br />
+        Get hyped about value
+      </CTABanner.Heading>
+    </CTABanner>
+    <IDE variant="glass" height={500}>
+      <IDE.Editor
+        showReplayButton={false}
+        files={[{
+          name: "Program.cs",
+          alternativeText: "A C# program",
+          code: `using System;
+
+Console.WriteLine("Let's write some code");`.split('\n')
+        }]}
+      />
+    </IDE>
+  </ThemeProvider>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
+import '@primer/react-brand/lib/css/main.css'
+import '@primer/react-brand/fonts/fonts.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
